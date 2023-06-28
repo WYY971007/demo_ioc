7878a58f0e531c48c7024f4de17b740050fd24ae
王韵扬 <1113581178@qq.com>
2023-06-28 18:19:33 +0800

    引入SpringContext依赖 使用spring IoC获取xml bean实例

--- src/main/java/com/demo/myspring/MyspringApplication.java
+++ src/main/java/com/demo/myspring/MyspringApplication.java
@@ -1,14 +1,26 @@
 package com.demo.myspring;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.demo.myspring.service.TransferService;
+import com.demo.myspring.spring.Factory.ProxyFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@SpringBootApplication
 public class MyspringApplication {
 
+
     public static void main(String[] args) {
-        SpringApplication.run(MyspringApplication.class, args);
+
+        //ioc容器指定加载xml文件
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        //根据id获取指定bean
+        TransferService transferService = (TransferService)applicationContext.getBean("transferService");
+        //获取代理工厂
+        ProxyFactory proxyFactory = (ProxyFactory)applicationContext.getBean("ProxyFactory");
+        //获取实际代理对象
+        TransferService jdkproxy = (TransferService)proxyFactory.getProxy(transferService);
+
+
     }
 
 }
